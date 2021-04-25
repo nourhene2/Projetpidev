812d81d94903f6ea7faa33cd59c8b9d5d6abae8d
malekkammoun <malek.kammoun@esprit.tn>
2021-04-25 03:24:48 +0200

    commit (trans)

---
+++ Desktop/pp/anglais.ts
@@ -0,0 +1,464 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="fr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>Gestion des Reservations</source>
+        <translation>reservation managmenet</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <source>Gestion des Commandes</source>
+        <translation> ordered managment</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <source> Ajouter une reservation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <location filename="mainwindow.ui" line="440"/>
+        <source>Ajout</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="96"/>
+        <location filename="mainwindow.ui" line="260"/>
+        <source>entrez l&apos;identifiant </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="109"/>
+        <location filename="mainwindow.ui" line="273"/>
+        <source>entrez le nombre de personnes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="122"/>
+        <source>entrez le numero du client</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>entrez la date de reservation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="188"/>
+        <source>Ajouter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="201"/>
+        <location filename="mainwindow.ui" line="365"/>
+        <location filename="mainwindow.ui" line="626"/>
+        <location filename="mainwindow.ui" line="912"/>
+        <source>retour</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="208"/>
+        <source>Afficher  une reservation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="230"/>
+        <source>pdf</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="236"/>
+        <source>Modifier une reservation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="248"/>
+        <location filename="mainwindow.ui" line="352"/>
+        <location filename="mainwindow.ui" line="887"/>
+        <source>Modifier</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="286"/>
+        <source>entrez le num de telephone du client</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="339"/>
+        <source>date de reservation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="372"/>
+        <source>Supprimer une reservation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="384"/>
+        <location filename="mainwindow.ui" line="856"/>
+        <source>supprimer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="397"/>
+        <source>entrez l&apos;identifiant de reservation que vous voulez la supprimer </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="428"/>
+        <source>Ajouter une commande</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="452"/>
+        <location filename="mainwindow.ui" line="789"/>
+        <location filename="mainwindow.ui" line="833"/>
+        <location filename="mainwindow.ui" line="925"/>
+        <source>code</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="465"/>
+        <location filename="mainwindow.ui" line="938"/>
+        <source>prix</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="478"/>
+        <location filename="mainwindow.ui" line="951"/>
+        <source>cp</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="491"/>
+        <location filename="mainwindow.ui" line="964"/>
+        <source>qt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="504"/>
+        <location filename="mainwindow.ui" line="977"/>
+        <source>num_telc</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="517"/>
+        <location filename="mainwindow.ui" line="750"/>
+        <location filename="mainwindow.ui" line="990"/>
+        <source>nom</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="530"/>
+        <location filename="mainwindow.ui" line="1003"/>
+        <source>datc</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="613"/>
+        <source>ajouter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="633"/>
+        <source>Afficher une commande</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="655"/>
+        <source>trie selon:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="668"/>
+        <source>CODE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="681"/>
+        <source>NOM</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="694"/>
+        <source>QUANTITE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="707"/>
+        <source>recherche: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="763"/>
+        <source>quantité</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="776"/>
+        <location filename="mainwindow.ui" line="802"/>
+        <location filename="mainwindow.ui" line="815"/>
+        <source>rechercher</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="821"/>
+        <source>Supprimer une commande</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="869"/>
+        <source>PushButton</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="875"/>
+        <source>Modifier une commande </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="899"/>
+        <source>modifier</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="1094"/>
+        <source>service client</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="commande.cpp" line="70"/>
+        <location filename="commande.cpp" line="100"/>
+        <location filename="commande.cpp" line="116"/>
+        <location filename="commande.cpp" line="132"/>
+        <location filename="commande.cpp" line="150"/>
+        <source>code</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="71"/>
+        <location filename="commande.cpp" line="101"/>
+        <location filename="commande.cpp" line="117"/>
+        <location filename="commande.cpp" line="133"/>
+        <location filename="commande.cpp" line="151"/>
+        <source>prix</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="72"/>
+        <location filename="commande.cpp" line="102"/>
+        <location filename="commande.cpp" line="118"/>
+        <location filename="commande.cpp" line="134"/>
+        <location filename="commande.cpp" line="152"/>
+        <source>cp</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="73"/>
+        <location filename="commande.cpp" line="103"/>
+        <location filename="commande.cpp" line="119"/>
+        <location filename="commande.cpp" line="135"/>
+        <location filename="commande.cpp" line="153"/>
+        <source>qt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="74"/>
+        <location filename="commande.cpp" line="104"/>
+        <location filename="commande.cpp" line="120"/>
+        <location filename="commande.cpp" line="136"/>
+        <location filename="commande.cpp" line="154"/>
+        <source>num_telc</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="75"/>
+        <location filename="commande.cpp" line="105"/>
+        <location filename="commande.cpp" line="121"/>
+        <location filename="commande.cpp" line="137"/>
+        <location filename="commande.cpp" line="155"/>
+        <source>nom</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="76"/>
+        <location filename="commande.cpp" line="106"/>
+        <location filename="commande.cpp" line="122"/>
+        <location filename="commande.cpp" line="138"/>
+        <location filename="commande.cpp" line="156"/>
+        <source>datc</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="184"/>
+        <location filename="commande.cpp" line="213"/>
+        <source>CODE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="185"/>
+        <location filename="commande.cpp" line="214"/>
+        <source>PRIX</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="186"/>
+        <location filename="commande.cpp" line="215"/>
+        <source>CP</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="187"/>
+        <location filename="commande.cpp" line="216"/>
+        <source>QT</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="188"/>
+        <location filename="commande.cpp" line="217"/>
+        <source>NUM_TELC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="189"/>
+        <location filename="commande.cpp" line="218"/>
+        <source>NOM</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="commande.cpp" line="190"/>
+        <location filename="commande.cpp" line="219"/>
+        <source>DATC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="24"/>
+        <location filename="mainwindow.cpp" line="78"/>
+        <location filename="mainwindow.cpp" line="82"/>
+        <location filename="mainwindow.cpp" line="114"/>
+        <location filename="mainwindow.cpp" line="167"/>
+        <location filename="mainwindow.cpp" line="171"/>
+        <location filename="mainwindow.cpp" line="229"/>
+        <source>database is open</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="24"/>
+        <source>connexion reussite
+click OK to exist</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="30"/>
+        <source>database is not open</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="30"/>
+        <source>connexion failed
+click Cancel to exist</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="72"/>
+        <location filename="mainwindow.cpp" line="108"/>
+        <location filename="mainwindow.cpp" line="161"/>
+        <location filename="mainwindow.cpp" line="223"/>
+        <source>vide</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="73"/>
+        <location filename="mainwindow.cpp" line="109"/>
+        <location filename="mainwindow.cpp" line="162"/>
+        <location filename="mainwindow.cpp" line="224"/>
+        <source>veuillez saisir tous les champs correctement!
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="79"/>
+        <source>reservation ajoutée:
+click ok to exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <source>reservation existe deja:
+click ok to exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="115"/>
+        <source>reservation modifiee:
+click ok to exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="168"/>
+        <source>commande ajoutée:
+click ok to exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="172"/>
+        <source>commande existe deja:
+click ok to exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="230"/>
+        <source>commande modifiee:
+click ok to exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="260"/>
+        <location filename="mainwindow.cpp" line="274"/>
+        <location filename="mainwindow.cpp" line="289"/>
+        <location filename="mainwindow.cpp" line="303"/>
+        <source>SEARCH</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="261"/>
+        <location filename="mainwindow.cpp" line="275"/>
+        <location filename="mainwindow.cpp" line="290"/>
+        <location filename="mainwindow.cpp" line="304"/>
+        <source>NO MATCH FOUND !.
+Click Cancel to exit.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="reservation.cpp" line="54"/>
+        <source>id_r</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="reservation.cpp" line="55"/>
+        <source>num_tel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="reservation.cpp" line="56"/>
+        <source>nbr_pr</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="reservation.cpp" line="57"/>
+        <source>dat</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
--- Desktop/pp/pp/pp.pro
+++ Desktop/pp/pp/pp.pro
@@ -31,4 +31,5 @@ qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
-RESOURCES +=
+RESOURCES += \
+    ressource.qrc
